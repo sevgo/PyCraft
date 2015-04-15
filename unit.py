3f277f2a7d9243a5bc8c9ae66c78f01482140e36
Nikolai Petkov <binana@abv.bg>
2015-04-15 20:46:55 +0300

    Add unit __init__ and tests

--- unit.py
+++ unit.py
@@ -1,1 +1,5 @@
 #!/usr/bin/env python3
+class Unit:
+    def __init__(self, health, mana):
+        self.health = health
+        self.mana = mana
--- unit_test.py
+++ unit_test.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+import unittest
+from unit import Unit
+
+
+class unit_test(unittest.TestCase):
+    def setUp(self):
+        self.unit = Unit(100, 100)
+
+    def test_init(self):
+        self.assertEqual(self.unit.health, 100)
+        self.assertEqual(self.unit.mana, 100)
+
+if __name__ == '__main__':
+    unittest.main()
