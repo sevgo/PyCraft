ace7db8f704e338c59a0b5d0f735bde7948a448d
Nikolai Petkov <binana@abv.bg>
2015-04-16 19:08:39 +0300

    Added dungeon printmap

--- dungeon.py
+++ dungeon.py
@@ -10,6 +10,14 @@ class Dungeon:
             if item in self.dungeon_map[i]:
                 return (i, self.dungeon_map[i].index(item))
 
+    def print_map(self):
+        for row in self.dungeon_map:
+            print(''.join(row))
+
     def spawn(self, hero):
         spawn_location = self._find_item_coordinates('S')
         self.dungeon_map[spawn_location[0]][spawn_location[1]] = 'H'
+
+if __name__ == '__main__':
+    dungeon = Dungeon('level1.txt')
+    dungeon.print_map()
