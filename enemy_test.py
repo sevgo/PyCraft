aac4387bce127637808e6dfa4efc942fc388d6c8
Nikolai Petkov <nikipetkov@abv.bg>
2015-04-17 15:35:16 +0300

    Fixed some typos in test

--- enemy_test.py
+++ enemy_test.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
-
-from unit import Unit
 from enemy import Enemy
 import unittest
 
 
 class Test_Enemy(unittest.TestCase):
     def setUp(self):
-        henry = Enemy(health=70, mana=45, damage=20)
+        self.henry = Enemy(health=70, mana=45, damage=20)
 
-    def Test_Enemy_Born(self):
+    def test_Enemy_Born(self):
         self.assertIsInstance(self.henry, Enemy)
         self.assertTrue(self.henry.mana == 45)
-        self.assertEqual(self.henry.mana_regeneration_rate, 0)
+        self.assertEqual(self.henry.mana_regeneration, 0)
 
 
 if __name__ == "__main__":
