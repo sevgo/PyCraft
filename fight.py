adb8dee96f19491a413f9046c8dba9faab2bc4d8
Nikolai Petkov <nikipetkov@abv.bg>
2015-04-20 16:58:22 +0300

    Some work of fight class

--- fight.py
+++ fight.py
@@ -7,36 +7,55 @@ from spell import Spell
 class Fight():
     def __init__(self, hero, enemy):
         self.hero = hero
-        self.hero.learn(Spell('a', 20, 20, 2))
         # hero = Hero()
         self.enemy = enemy
-        self.enemy.learn(Spell('b', 25, 20, 2))
         # enemy = Enemy()
 
     def start(self):
         start_battle = "A fight is started between our Hero(health={}, mana={})"
-        start_battle += " and Enemey(health={}, mana={}, damage={})"
+        start_battle += " and Enemy(health={}, mana={}, damage={})"
         print(start_battle.format(self.hero.get_health(), self.hero.get_mana(),
               self.enemy.get_health(), self.enemy.get_mana(), self.enemy.damage))
         while self.hero.is_alive() and self.enemy.is_alive():
-            hero_attack = self.hero.attack()
-            self.enemy.take_damage(hero_attack[1])
-            if hero_attack[0] == 'spell':
-                spell_text = 'Hero casts a {}, hits enemy for {} dmg. Enemy health is {}'
-                print(spell_text.format(self.hero.spell.name, hero_attack[1], enemy.health))
-            if self.enemy.is_alive():
-                enemy_attack = self.enemy.attack()
-                if enemy_attack[0] == 'spell':
-                    spell_text = 'Enemy casts a {}, hits hero for {} dmg. Hero health is {}'
-                    print(spell_text.format(self.enemy.spell.name, enemy_attack[1], enemy.health))
-                self.hero.take_damage(enemy_attack[1])
+            if self.hero.can_cast():
+                spell = "Hero casts a {}, hits enemy for {} dmg. Enemy health is {}"
+                damage = self.hero.attack(by='spell')
+                self.enemy.take_damage(damage)
+                print(spell.format(self.hero.spell.name,
+                                   self.hero.spell.damage, self.enemy.health))
+            elif self.hero.weapon != None:
+                weapon = "Hero hits with {} for {} dmg. Enemy health is {}"
+                damage = self.hero.attack(by='weapon')
+                self.enemy.take_damage(damage)
+                print(weapon.format(self.hero.weapon.name,
+                                    self.hero.weapon.damage, self.enemy.health))
+            if enemy.is_alive():
+                if self.enemy.can_cast():
+                    spell = "Enemy casts a {}, hits hero for {} dmg. Hero health is {}"
+                    damage = self.enemy.attack(by='spell')
+                    self.hero.take_damage(damage)
+                    print(spell.format(self.hero.spell.name,
+                                       self.hero.spell.damage, self.enemy.health))
+                elif self.enemy.weapon != None:
+                    weapon = "Enemy hits with {} for {} dmg. Hero health is {}"
+                    damage = self.enemy.attack(by='weapon')
+                    self.hero.take_damage(damage)
+                    print(weapon.format(self.hero.weapon.name,
+                                        self.hero.weapon.damage, self.enemy.health))
+                else:
+                    hands = "Enemy hits hero for {} dmg. Hero health is {}."
+                    damage = self.enemy.attack()
+                    self.hero.take_damage(damage)
+                    print(hands.format(self.enemy.damage, self.hero.health))
+
         return self.hero if self.hero.is_alive() else self.enemy
 
 
 if __name__ == '__main__':
-    weapon = Weapon('name', 2)
-    spell = Spell('name', 0, 0, 0)
+    weapon = Weapon('WW', 2)
+    spell = Spell('SS', 5, 40, 2)
     hero = Hero()
     hero.equip(weapon)
+    hero.learn(spell)
     enemy = Enemy()
     Fight(hero, enemy).start()
